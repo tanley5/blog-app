3146d56989efa58c3e047e90746d7176386f3cfa
tanley5 <tanleybench@gmail.com>
2019-10-13 01:16:11 -0600

    Added CRUD functionality

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -7,6 +8,9 @@ class Post(models.Model):
         on_delete=models.CASCADE,
     )
     body = models.TextField()
+
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[str(self.id)])
 
     def __str__(self):
         return self.title
